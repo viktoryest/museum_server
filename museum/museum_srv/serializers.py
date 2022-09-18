ab1414ab2d7ccd3b27f61f87f6eb9a15eaa7b8ab
viktoryest <67237779+viktoryest@users.noreply.github.com>
2022-09-18 18:04:09 +0400

    Changes employee table, creates methods for video stand page
    Creates employees model, methods for video stand page, method for viewing different categories of employee

--- museum/museum_srv/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import VideoStandPage
-
-
-class VideoStandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VideoStandPage
-        fields = '__all__'
